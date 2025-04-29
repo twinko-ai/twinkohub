db089fb9d484e44a5c2eefd6d2956c0efb5b9327
tengfei <yintengfei@gmail.com>
2025-04-29 16:47:52 -0400

    fix netfliy config

--- publishconf.py
+++ publishconf.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
+# This file is only used if you use `make publish` or explicitly specify it as your config file.
+
 from pelicanconf import *
 
-# Netlify site URL
+# Production URL
 SITEURL = 'https://twinkohub.netlify.app'
 RELATIVE_URLS = False
 
 # Feed settings
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
+# Delete output directory before generating new files
 DELETE_OUTPUT_DIRECTORY = True
